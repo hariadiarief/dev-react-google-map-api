40ecb4d2183d1896232314fe0715471848ace141
hariadiarief <adi.arief77@gmail.com>
2023-03-02 23:25:42 +0700

    install: tailwind

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ export default function App() {
   const selectedLocation = useSelector((state) => state.googleMap.selectedLocation)
 
   return (
-    <div>
-      <InputPlaceAutocomplete placeholder='input search text' style={{ width: '100%' }} />
-      <iframe
-        title='map'
-        width="600"
-        height="450"
-        loading="lazy"
-        src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_API_KEY}&q=""${!selectedLocation ? '' : encodeURI(selectedLocation)}`}
-      />
-    </div>
+    <div className={`p-8 ${!selectedLocation && 'grid place-items-center h-screen'}`}>
+      <InputPlaceAutocomplete placeholder='Search your location' style={{ width: '100%' }} />
+      {
+        !selectedLocation
+          ? null
+          : <iframe
+            className="mt-8"
+            title='map'
+            width="100%"
+            height="450"
+            loading="lazy"
+            src={`https://www.google.com/maps/embed/v1/place?key=${process.env.REACT_APP_GOOGLE_API_KEY}&q=""${!selectedLocation ? '' : encodeURI(selectedLocation)}`}
+          />
+      }
+
+    </div >
   )
 }
